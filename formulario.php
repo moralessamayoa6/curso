b2b0254d7d29a7890aa0c4fe8b32975e25efefbd
moralessamayoa6 <moralessamayoa6@gmail.com>
2018-11-12 11:34:55 -0600

    agrege formulario

---
+++ formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Guardar</title>
+</head>
+<boby>
+	<center>
+		<form action="guardar.php" method="POST">
+			<br/><br/><br/>
+			
+			<input type="text" required name="nombre" placeholder="nombre..." value="" /><br/><br/>
+			<input type="text" required name="apellido" placeholder="apellido..." value="" /><br/><br/>
+			<input type="submit" value="Aceptar" />
+		</form>
+	</center>
+</boby>
+</html>
